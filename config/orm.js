5da650817206867510abf0b88d8f4dbd6f4ffb60
Joseph Holder <josephholder@Josephs-MacBook-Pro.local>
2018-05-14 18:23:50 -0400

    orm

---
+++ config/orm.js
@@ -0,0 +1,38 @@
+var connection = require("../config/connection.js");
+
+// SQL function statements
+var orm = {
+    //select all burgers
+    selectAll: function (table, cb) {
+        var queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [table], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    //insert a new burger
+    insertOne: function (table, burger_name, devoured, cb) {
+        var queryString = "INSERT INTO ?? VALUES (?, ?);";
+        connection.query(queryString, [table, burger_name, devoured], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    //update a burgers devoured status
+    update: function (table, devoured, id, cb) {
+        var queryString = "UPDATE ?? SET Devoured ? WHERE ID= ?;";
+        connection.query(queryString, [table, devoured, id], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+// Export the orm object
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,19 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (results) {
+            cb(results);
+        })
+    },
+    insertOne: function (burger_name, devoured, cb) {
+        orm.insertOne("burgers", burger_name, devoured, cb, function (results) {
+            cb(results);
+        })
+    },
+    updateOne: function (id, devoured, cb) {
+        orm.updateOne("burgers", id, devoured, function (result) {
+            cb(result);
+        })
+    }
+};
